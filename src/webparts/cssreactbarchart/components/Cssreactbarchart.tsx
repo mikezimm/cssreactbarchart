c3bd8fe77a6755251e5e218667567d6532b66cc9
mikezimm <49648086+mikezimm@users.noreply.github.com>
2020-09-19 21:57:52 -0400

    Update chart styling and options

--- src/webparts/cssreactbarchart/components/Cssreactbarchart.tsx
+++ src/webparts/cssreactbarchart/components/Cssreactbarchart.tsx
@@ -5,6 +5,7 @@ import { escape } from '@microsoft/sp-lodash-subset';
 
 import { getRandomInt, getRandomFromArray, randomDate, getRandomChance } from '../../../services/randomServices';
 
+import { ICSSChartSeries } from './IReUsableInterfaces';
 
 import stylesC from './cssChart.module.scss';
 
@@ -14,109 +15,173 @@ export interface ISimpleData {
   perc: number;
 }
 
-export function chartData( vals: number[], titles: string[]) {
+export function makeChartData( qty: number, label: string ) {
 
-  const arrSum = vals.reduce((a,b) => a + b, 0);
-  let dataArray: ISimpleData[] = [];
-
-  let i = 0;
-
-  vals.map( v => {
-    dataArray.push({ 
-      title: titles[i],
-      value: v,
-      perc: v / arrSum * 100,
-    });
-    i++;
-  });
-
-  return dataArray;
+  let randomNums = generateVals(qty);
+  let randomTitles = generateTitles( label, qty );
+  const arrSum = randomNums.reduce((a,b) => a + b, 0);
+  let percents = randomNums.map( v => { return (v / arrSum * 100 ) ; });
+  let chartData: ICSSChartSeries = {
+    title: label,
+    labels: randomTitles,
+    counts: randomNums,
+    percents: percents,
+    totalS: arrSum,
+  };
+  return chartData;
 }
 
+
+export function generateVals ( qty ) {
+  let vals = [];
+  for (let i = 0; i < qty ; i++) {
+    vals.push (  getRandomInt(11 , 75) );
+  }
+  return vals;
+}
+
+export function generateTitles ( lbl: string, qty: number ) {
+  let titles = [];
+  for (let i = 0; i < qty ; i++) {
+    //https://stackoverflow.com/a/3145054
+    var chr = String.fromCharCode(65 + i);
+    titles.push (  lbl + ' - ' + chr );
+  }
+  return titles;
+}
+
+export function sortKeysByOtherKey( obj: any, sortKey: string, order: 'asc' | 'dec', dataType: 'number' | 'string', otherKeys: string[]) {
+
+  let sortCopy : number[] | string[] = JSON.parse(JSON.stringify(obj[sortKey]));
+
+  let otherKeyArrays : any = {};
+  otherKeys.map( m => { otherKeyArrays[m] = [] ; } );
+  if ( order === 'asc' ) {
+    sortCopy.sort();
+  } else {
+    sortCopy.sort((a, b) => { return b-a ;});
+  }
+  
+  
+  let x = 0;
+  for ( let v of sortCopy) {
+    let currentIndex = obj[sortKey].indexOf(v); //Get index of the first sortable value in original array
+    let i = 0;
+    otherKeys.map( key => {
+      otherKeyArrays[key].push( obj[key][currentIndex] );
+    });
+    obj[sortKey][currentIndex] = null;
+    x ++;
+  }
+
+  otherKeys.map( key => {
+    obj[key] = otherKeyArrays[key] ;
+  }); 
+
+  return obj;
+
+}
+
+const chartType: 'bar' | 'other' = 'bar';
+const stacked: boolean = false;
+const sortStack: 'asc' | 'dec' | false = undefined;
+const barValueAsPercent : boolean = false;
+const height: number | string = "50px"; //This would be horizonal bar height... one horizontal layer
+const barValues: 'counts' | 'sums' | 'avgs' | 'percents' = 'counts';
+
 export default class Cssreactbarchart extends React.Component<ICssreactbarchartProps, {}> {
-
-  private generateVals ( qty ) {
-    let vals = [];
-    for (let i = 0; i < qty ; i++) {
-      vals.push (  getRandomInt(11 , 75) );
-    }
-    return vals;
-  }
-
-  private generateTitles ( lbl: string, qty: number ) {
-    let titles = [];
-    for (let i = 0; i < qty ; i++) {
-      //https://stackoverflow.com/a/3145054
-      var chr = String.fromCharCode(65 + i);
-      titles.push (  lbl + ' - ' + chr );
-    }
-    return titles;
-  }
 
   public render(): React.ReactElement<ICssreactbarchartProps> {
 
     // Styles & Chart code for chart compliments of:  https://codepen.io/richardramsay/pen/ZKmQJv?editors=1010
 
-    let data1 : ISimpleData[] = chartData(this.generateVals(10), this.generateTitles( 'Category', 10 ));
+    let chartData: ICSSChartSeries[] = [];
 
-    let data2 : ISimpleData[] = chartData(this.generateVals(10), this.generateTitles( 'Item', 10 ));
+    chartData.push( makeChartData(10, 'Category') ) ;
+    chartData.push( makeChartData(10, 'Item') ) ;
+    chartData.push( makeChartData(10, 'Product') ) ;
 
-    let data3 : ISimpleData[] = chartData(this.generateVals(10), this.generateTitles( 'Product' , 10 ));
+    console.log('chartData Before: ', chartData );
+    if ( stacked === false ) {
+      //Re-sort all arrays by same key:
 
-    let chart1 = data1.map( d => {
-        return <span className={ [stylesC.block, stylesC.innerShadow].join(' ') } style={{ width: (d.perc) + '%'  }} title={ d.title } >
-        <span className={ stylesC.value } >{ d.value }%</span>
-      </span>;
-      });
+    }
 
-      let chart2 = data2.map( d => {
-        return <span className={ [stylesC.block, stylesC.innerShadow].join(' ') } style={{ width: (d.perc) + '%'  }} title={ d.title } >
-        <span className={ stylesC.value } >{ d.value }%</span>
-      </span>;
-      });
+    let stateHeight = stacked === false ? "40px" : height;
 
-      let chart3 = data3.map( d => {
-        return <span className={ [stylesC.block, stylesC.innerShadow].join(' ') } style={{ width: (d.perc) + '%'  }} title={ d.title } >
-        <span className={ stylesC.value } >{ d.value }%</span>
-      </span>;
-      });
+    let charts = chartData.map( cdO => {
 
+      let cd : ICSSChartSeries = null;
 
+      if ( stacked === false || sortStack === 'asc' || sortStack === 'dec' ) {
+        let sortOrder : 'asc' | 'dec' = stacked === false || sortStack === 'dec' ? 'dec' : 'asc';
+        cd = sortKeysByOtherKey( cdO, barValues, sortOrder, 'number', ['labels',barValues,'percents'] );
+      } else {
+        cd = cdO;
+      }
+
+      console.log('chartData after: cd', cd );
+
+      let thisChart : any[] = [];
+      let maxNumber: number = Math.max( ...cd[barValues] );  //Need to use ... spread in math operators:  https://stackoverflow.com/a/1669222
+      for ( let i in cd[barValues] ){
+
+        let blockStyle : any = { height: stateHeight , width: ( cd.percents[i] ) + '%'};
+        let valueStyle : any = {};
+        let barLabel = barValueAsPercent === true ? ( cd.percents[i].toFixed(1) ) + '%' : cd[barValues][i];
+        if ( stacked === false ) { 
+          let barPercent = ( cd[barValues][i] / maxNumber ) * 100;
+          blockStyle.float = 'none' ;
+          blockStyle.width = barPercent + '%';
+          barLabel += ' - ' + cd.labels[i];
+          blockStyle.whiteSpace = 'nowrap';
+
+          if ( barPercent < 50 ) {
+            blockStyle.overflow = 'visible';
+            let leftValue = barPercent < 1 ? '200px' : 140 + ( 50 - barPercent ) * 20 / barPercent  + '%'; 
+            valueStyle.left = leftValue;
+            blockStyle.color = 'black';
+          }
+
+        }
+
+        thisChart.push(
+          <span className={ [stylesC.block, stylesC.innerShadow].join(' ') } style={ blockStyle } title={ cd.labels[i] } >
+              <span className={ stylesC.value } style={ valueStyle } >{ barLabel }</span>
+          </span>
+        ) ;
+      }
+
+      let chartStyles : any = { lineHeight: stateHeight };
+      let rowStyles : any = stacked === false ? { maxWidth: '450px' } : {};
+
+      return <div className={ stylesC.row } style={ rowStyles }>
+          <h6 style={ chartStyles }>{ cd.title }</h6>
+          <div className={ stylesC.chart } >
+            { thisChart }
+          </div>
+        </div>;
+
+    });
 
     return (
       <div className={ styles.cssreactbarchart }>
         <div className={ styles.container }>
           <figure className={ stylesC.cssChart }>
             <div className={ stylesC.yAxis } >
-              <h3>Y-Axis Title</h3>
+              <h3>Chart Title</h3>
             </div>
-
             <div className={ stylesC.graphic } >
-
-              <div className={ stylesC.row } >
-                <h6>Bar One</h6>
-                <div className={ stylesC.chart } >
-                  { chart1 }
-                </div>
-              </div>
-
-              <div className={ stylesC.row } >
-                <h6>Bar Two</h6>
-                <div className={ stylesC.chart } >
-                  { chart2 }
-                </div>
-              </div>
-
-              <div className={ stylesC.row } >
-                <h6>Bar Three</h6>
-                <div className={ stylesC.chart } >
-                  { chart3 }
-                </div>
-              </div>
-
+              { charts }
             </div>
+          </figure>
+        </div>
+      </div>
+    );
+  }
 
-            <div className={ stylesC.xAxis } >
+  /**   This is the legend code:
+   *        <div className={ stylesC.xAxis } >
               <h3>X-Axis Title</h3>
               <ul className={ stylesC.legend } >
                 <li>Category A</li>
@@ -127,12 +192,9 @@ export default class Cssreactbarchart extends React.Component<ICssreactbarchartP
                 <li>Category F</li>
               </ul>
             </div>
+   */
 
-          </figure>
-        </div>
-      </div>
-    );
-  }
+
 }
 
 
